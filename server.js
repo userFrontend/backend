1324535b14958d553b8ada4e346ed5ce3defeb37
userFrontend <aba06096@gmail.com>
2023-12-20 19:31:18 +0500

    edited

--- server.js
+++ server.js
@@ -44,32 +44,38 @@ app.use('/api/message', messageRouter);
 // websocket functions
 let activeUsers = [];
 
-io.on('connection', (socket) => {
-    socket.on('new-user-add', (newUserId) => {
-        if(!activeUsers.some(user => user.userId === newUserId)){
-            activeUsers.push({userId: newUserId, socketId: socket.id})
-        }
+io.on("connection", (socket) => {
+    socket.on("new-user-added", (newUserId) => {
+      if (!activeUsers.some((user) => user.userId === newUserId)) {
+        activeUsers.push({ userId: newUserId, socketId: socket.id });
+      }
+  
+      io.emit("get-users", activeUsers);
+    });
+  
+    socket.on("disconnect", () => {
+      activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+  
+      io.emit("get-users", activeUsers);
+    });
+  
+    socket.on("exit", (id) => {
+      activeUsers = activeUsers.filter((user) => user.userId !== id);
+  
+      io.emit("get-users", activeUsers);
+    });
+  
+    socket.on("send-message", (data) => {
+        console.log(data);
+        const { receivedId } = data;
+        const user = activeUsers.find((user) => user.userId === receivedId);
+        if (user) {
+            io.to(user.socketId).emit("answer-message", data);
+            console.log('ketdi');
+      }
+    });
+  });
 
-        io.emit('get-users', activeUsers)
-    })
-
-    socket.on('disconnect', () => {
-        activeUsers = activeUsers.filter(user => user.socketId !== socket.id)
-        io.emit('get-users', activeUsers)
-    })
-
-    socket.on('exit', (id) => {
-        activeUsers = activeUsers.filter(user => user.userId !== id)
-       io.emit('get-users', activeUsers)
-    })
-    socket.on('send-message', (data) => {
-        const {receivedId} = data
-        const user = activeUsers.find(user => user.userId === receivedId)
-        if(user){
-            io.to(user.socketId).emit('answer-message', data)
-        }
-    })
-})
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL, {})
